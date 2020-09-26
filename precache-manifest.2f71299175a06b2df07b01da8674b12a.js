13c253185830a10306787d6d4cba2c6299b43d65
Paola Garay <garay.angela@gmail.com>
2020-09-26 20:02:35 +0200

    Updates

---
+++ precache-manifest.2f71299175a06b2df07b01da8674b12a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e97dba3e77e1f05e87e99595b0f9d5",
+    "url": "/react-video-browser/index.html"
+  },
+  {
+    "revision": "060f2ce4affcd2d3d385",
+    "url": "/react-video-browser/static/css/main.6207871e.chunk.css"
+  },
+  {
+    "revision": "3ca71b351f4a2fbf27b5",
+    "url": "/react-video-browser/static/js/2.de1ca534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-video-browser/static/js/2.de1ca534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060f2ce4affcd2d3d385",
+    "url": "/react-video-browser/static/js/main.264eebd7.chunk.js"
+  },
+  {
+    "revision": "401702ca670f40986d42",
+    "url": "/react-video-browser/static/js/runtime-main.77c10d7d.js"
+  }
+]);
